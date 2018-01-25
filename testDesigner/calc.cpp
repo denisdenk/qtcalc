3d07caaebb84ba493a8fb8bf09ddca119289464d
denisdenk <denisdenk@live.ru>
2018-01-25 21:40:21 +0100

    Add slots

--- testDesigner/calc.cpp
+++ testDesigner/calc.cpp
@@ -12,3 +12,133 @@ Calc::~Calc()
 {
     delete ui;
 }
+
+void Calc::on_clearButton_released()
+{
+
+}
+
+void Calc::on_hexButton_released()
+{
+
+}
+
+void Calc::on_decButton_released()
+{
+
+}
+
+void Calc::on_octButton_released()
+{
+
+}
+
+void Calc::on_sqrtButton_released()
+{
+
+}
+
+void Calc::on_powButton_released()
+{
+
+}
+
+void Calc::on_substractButton_released()
+{
+
+}
+
+void Calc::on_plusButton_released()
+{
+
+}
+
+void Calc::on_multiplyButton_released()
+{
+
+}
+
+void Calc::on_equallyButton_released()
+{
+
+}
+
+void Calc::on_divideButton_released()
+{
+
+}
+
+void Calc::on_dotButton_released()
+{
+
+}
+
+void Calc::on_mcButton_released()
+{
+
+}
+
+void Calc::on_mrButton_released()
+{
+
+}
+
+void Calc::on_msButton_released()
+{
+
+}
+
+void Calc::on_mplusButton_released()
+{
+
+}
+
+void Calc::on_zeroButton_released()
+{
+
+}
+
+void Calc::on_oneButton_released()
+{
+
+}
+
+void Calc::on_twoButton_released()
+{
+
+}
+
+void Calc::on_threeButton_released()
+{
+
+}
+
+void Calc::on_fourButton_released()
+{
+
+}
+
+void Calc::on_fiveButton_released()
+{
+
+}
+
+void Calc::on_sixButton_released()
+{
+
+}
+
+void Calc::on_sevenButton_released()
+{
+
+}
+
+void Calc::on_eightButton_released()
+{
+
+}
+
+void Calc::on_nineButton_released()
+{
+
+}
